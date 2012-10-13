fa61fa0b0193af37a89d033786af4150cd3067d6
Daniel Spencer <dspencer@squareup.com>
2012-10-13 11:27:33 -0700

    Shows the ship correctly.

--- player.rb
+++ player.rb
@@ -1,6 +1,6 @@
 class Player
   def initialize(window)
-    @image = Gosu::Image.new(window, "ships.png", false)
+    @image = Gosu::Image.load_tiles(window, "ships.png", 26, 18, false)[image_offset]
     @x = @y = @vel_x = @vel_y = @angle = 0.0
     @score = 0
   end
@@ -10,16 +10,16 @@ class Player
   end
 
   def turn_left
-    @angle -= 4.5
+    @angle -= turn_speed
   end
 
   def turn_right
-    @angle += 4.5
+    @angle += turn_speed
   end
 
   def accelerate
-    @vel_x += Gosu::offset_x(@angle, 0.5)
-    @vel_y += Gosu::offset_y(@angle, 0.5)
+    @vel_x += Gosu::offset_x(@angle, acceleration)
+    @vel_y += Gosu::offset_y(@angle, acceleration)
   end
 
   def move
@@ -28,11 +28,23 @@ class Player
     @x %= 640
     @y %= 480
 
-    @vel_x *= 0.95
-    @vel_y *= 0.95
+    # @vel_x *= 0.95
+    # @vel_y *= 0.95
   end
 
   def draw
     @image.draw_rot(@x, @y, 1, @angle)
   end
+
+  # TODO override these methods
+  def image_offset
+    rand(100)
+  end
+  def turn_speed
+    4.5
+  end
+  def acceleration
+    0.1
+  end
+
 end
